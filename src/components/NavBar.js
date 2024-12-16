9e841807a2cc8d6eb106a85709d13fa614c5cab7
Michael Sturgeon <1993msturgeon@gmail.com>
2024-12-16 14:57:02 +0000

    Add & wire up NavBar links using React-Router library

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,18 +4,21 @@ import React from 'react'
 import { Navbar, Nav, Container } from "react-bootstrap"
 import logo from "../assets/logo.png"
 import styles from "../styles/NavBar.module.css"
+import {NavLink} from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
         <Container>
-           <Navbar.Brand><img src={logo} alt='logo' height="45px" /></Navbar.Brand>
+          <NavLink to="/">
+            <Navbar.Brand><img src={logo} alt='logo' height="45px" /></Navbar.Brand>
+          </NavLink>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto text-left">
-                    <Nav.Link><i className="fas fa-home"></i>Home</Nav.Link>
-                    <Nav.Link><i className="fas fa-sign-in-alt"></i>Sign-In</Nav.Link>
-                    <Nav.Link><i className="fas fa-user-plus"></i>Sign-Out</Nav.Link>
+                    <NavLink className={styles.NavLink} activeClassName={styles.Active} exact to="/"><i className="fas fa-home"></i>Home</NavLink>
+                    <NavLink className={styles.NavLink} activeClassName={styles.Active} exact to="/signin"><i className="fas fa-sign-in-alt"></i>Sign-In</NavLink>
+                    <NavLink className={styles.NavLink} activeClassName={styles.Active} exact to="/signup"><i className="fas fa-user-plus"></i>Sign-Up</NavLink>
                 </Nav>
             </Navbar.Collapse> 
         </Container>        
