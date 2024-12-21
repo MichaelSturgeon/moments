a143b05681a59a51f425f1efff93aedbd6d01fc5
Michael Sturgeon <1993msturgeon@gmail.com>
2024-12-21 18:04:24 +0000

    Add signin/ signout functionality. Create function to close the hamburger menu on mouseup

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://drf-api-w-project-2b5a13ae600c.herokuapp.com/";
+axios.defaults.baseURL = "https://drf-api-w-project-2b5a13ae600c.herokuapp.com";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
